baefe59ded8ad293c3338c225dd467e3df5a0fad
thelonehegelian <thelonehegelian@protonmail.com>
2025-03-17 11:36:47 +0000

    wrap in try catch

--- frontend/src/components/AdminLogin/actions.ts
+++ frontend/src/components/AdminLogin/actions.ts
@@ -1,5 +1,5 @@
 // for more info on how this works see: https://supabase.com/docs/guides/auth/server-side/nextjs
-// NOTE: this can be an api route or a server action
+// NOTE: this can be an api
 'use server'
 
 import { createClient } from '@/utils/supabase/server'
@@ -8,15 +8,23 @@ import { redirect } from 'next/navigation'
 export async function loginWithOtp(formData: FormData) {
   const supabase = await createClient()
   const email = formData.get('email') as string
+  console.log('NEXT_PUBLIC_SITE_URL:', process.env.NEXT_PUBLIC_SITE_URL)
   
-  const { error } = await supabase.auth.signInWithOtp({
-    email,
-    options: {
-      emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL ?? 'http://localhost:3000'}/auth/callback`,
-    },
-  })
-  
-  return { error }
+  try {
+    const { error } = await supabase.auth.signInWithOtp({
+      email,
+      options: {
+        emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL ?? 'http://localhost:3000'}/auth/callback`,
+      },
+    })
+    if (error) {
+      console.error('Error during OTP sign-in:', error.message)
+      return { error }
+    }
+  } catch (err) {
+    console.error('Unexpected error during OTP sign-in:', err)
+    return { error: err }
+  }
 }
 
 export async function logoutUser() {
