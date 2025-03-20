25ef2efef8a96d80559cc235f32ffca017ec8a93
Dave Balderstone <dave@poteris.co.uk>
2025-03-20 17:27:57 +0000

    OCPB-143. Change use of Request to NextRequest

--- frontend/app/api/persona/generate-new-persona/route.ts
+++ frontend/app/api/persona/generate-new-persona/route.ts
@@ -1,5 +1,5 @@
 import { generateNewPersona as newPersona } from "@/lib/server/services/persona/generateNewPersona";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 const personaSchema = z.object({
@@ -18,7 +18,7 @@ const personaSchema = z.object({
   emotional_conditions: z.string(),
 });
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   try {
     const persona = await newPersona(request.headers);
 
